f9573a4110107f7dc1ab01feef6e866791bdd900
CDeshechka <deniscvetkove@gmail.com>
2026-02-10 19:06:43 +0300

    Создал модель и класс описания школьников

---
+++ OGE/OGE/Model/schoolchildren.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace OGE.Model
+{
+    public class schoolchildren
+    {
+        public string firstname {  get; set; }
+        public string lastname { get; set; }
+        public int age { get; set; }
+        public string email { get; set; }
+        public DateTime dateofbirthday { get; set; }
+    }
+}
