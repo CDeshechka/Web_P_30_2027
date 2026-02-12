947d9948b8f52fd76ec25a7a86ebb01c03681f93
Viatlyyy <krilow.vitaly007@gmail.com>
2026-02-12 09:25:39 +0300

    Создание моделей EFModel и предмет, а также класса с их описанием, изменение всех наименоввани с большой буквы

---
+++ OGE/OGE/Model/Subject.cs
@@ -0,0 +1,11 @@
+﻿namespace OGE.Model
+{
+    public class Subject
+    {
+            public string Subjectname { get; set; }
+            public double Assessmentfortheoge { get; set; }
+            public double Academicyearassessment { get; set; }
+            public double Finalassessment { get; set; }
+    }
+}
+
--- OGE/OGE/Model/schoolchildren.cs
+++ OGE/OGE/Model/schoolchildren.cs
@@ -2,12 +2,12 @@
 
 namespace OGE.Model
 {
-    public class schoolchildren
+    public class Schoolchildren
     {
-        public string firstname {  get; set; }
-        public string lastname { get; set; }
-        public int age { get; set; }
-        public string email { get; set; }
-        public DateTime dateofbirthday { get; set; }
+        public string Firstname {  get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public DateTime Dateofbirthday { get; set; }
     }
 }
