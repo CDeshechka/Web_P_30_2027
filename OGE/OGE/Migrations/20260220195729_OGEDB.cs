8d564a23382994b3b736bcb35283f0d194b23d13
CDeshechka <deniscvetkove@gmail.com>
2026-02-20 23:07:09 +0300

    Добавил новую миграцию

---
+++ OGE/OGE/Migrations/20260220195729_OGEDB.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OGE.Migrations
+{
+    /// <inheritdoc />
+    public partial class OGEDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Schoolchildrens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Firstname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Lastname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dateofbirthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schoolchildrens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subject1",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Assessmentfortheoge = table.Column<double>(type: "float", nullable: false),
+                    Academicyearassessment = table.Column<double>(type: "float", nullable: false),
+                    Finalassessment = table.Column<double>(type: "float", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subject1", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subject2",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Auditoriumnumber = table.Column<double>(type: "float", nullable: false),
+                    Auditoriumcapacity = table.Column<double>(type: "float", nullable: false),
+                    Auditoriumsubject = table.Column<double>(type: "float", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subject2", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Schoolchildrens");
+
+            migrationBuilder.DropTable(
+                name: "Subject1");
+
+            migrationBuilder.DropTable(
+                name: "Subject2");
+        }
+    }
+}
